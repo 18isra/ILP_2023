fc82c67bf75f48eec8cacad5b1c72cc0543e812a
18isra <123257672+18isra@users.noreply.github.com>
2023-01-21 11:20:29 -0600

    Add files via upload
    EJEMPLO 1

---
+++ ILP_2023/ejemplo1.py
@@ -0,0 +1,11 @@
+# ejemplo 1. saludo
+# comentarios: sirven para detallar y especificar cada parte del codigo 
+# a los comentarios los omite o no toma en cuenta python
+# imprimir en pantalla o mostrar resultados 
+
+print(5+5) #suma
+print(10-2)#resta
+print("hola mundo")
+print("israel");
+print(7*8)
+
