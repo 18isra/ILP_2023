cb6c58bebe3588047f96c5e0f5d77079bd9ce985
18isra <123257672+18isra@users.noreply.github.com>
2023-01-21 14:09:27 -0600

    ejemplo 2

---
+++ ILP_2023/Ejemplo2.py
@@ -0,0 +1,50 @@
+#operaciones matematicas
+
+#entrada de datos
+#declarar o crear dos variables
+numero1= 8
+numero2= 7
+
+#constante
+PI=3.14
+
+
+
+#procesos (operaciones o calculos matematicos y logicos)
+suma=numero1+numero2
+resta=numero1-numero2
+multiplicacion=numero1*numero2
+division=numero1/numero2
+
+potencia1=numero1**numero2
+potencia2=pow(numero1,numero2)
+cuadrado=numero1**2
+cubo=pow(numero2,3)
+
+raiz_cuadrada1=numero1**0.5
+raiz_cuadrada2=pow(numero1,1/2)
+raiz_cubica1=pow(numero1,1/3)
+raiz_cubica2=pow(numero1,1/3)
+
+modulo_residuo1=8%7
+modulo_rediduo2=7%8
+
+
+
+#salidad de datos
+print("la suma es igual a",suma)
+print("la resta es igual a",resta)
+print("la multiplicacion es igual a",multiplicacion)
+print("la division es igual a",division)
+
+print(f"la suma es igual a {suma}")
+
+print("la potencia1=",potencia1)
+print("la potencia 2=",potencia2)
+print("la raiz cuadrada1=",raiz_cuadrada1)
+print("la raiz cuadrada2=",raiz_cuadrada2)
+print("la raiz cubica1=",raiz_cubica1)
+print("la raiz cubica2=",raiz_cubica2)
+
+print("modulo residuo1=",modulo_residuo1)
+print("modulo residuo2=",modulo_rediduo2)
