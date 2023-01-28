36d7471cd513c3119d71a3cdc41d22f2f82acfdf
18isra <123257672+18isra@users.noreply.github.com>
2023-01-28 11:12:52 -0600

    EJERCICIO 2

---
+++ Ejercicio2.py
@@ -0,0 +1,19 @@
+# Ejercicio 2 Calcular el área y perímetro de un círculo.
+
+#entrada de datos 
+radio= float(input("escribe radio: "))
+area= 0
+perimetro= 0
+
+#constante
+PI=3.14
+
+
+
+#procesos 
+area= (PI*radio)**2
+perimetro= 2*PI*radio
+
+#salidas 
+print("escribe area circulo: ", area)
+print("escribe perimetro circulo: ", perimetro)
