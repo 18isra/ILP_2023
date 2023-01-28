3fda5721c7924377e711d78cda83f17cca4e308d
18isra <123257672+18isra@users.noreply.github.com>
2023-01-28 15:27:32 -0600

    EJERCICIO 3

---
+++ Ejercicio3.py
@@ -0,0 +1,14 @@
+#Ej Determinar la edad de una persona conociendo el año actual y el año de nacimiento.
+
+#entrada
+añoactual= 2023
+edadpersona= int(input("escribe año de nacimiento: "))
+añonacimiento= 1935
+
+#proceso
+resta= añoactual-añonacimiento
+
+#salida
+print("edad de persona es:",resta)
+
+
