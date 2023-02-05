e9f3243c3aaa2eae9e2a42fdc4d2b4c046698d16
18isra <123257672+18isra@users.noreply.github.com>
2023-02-04 18:09:45 -0600

    EJERCICIO 1 COMPLETO

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -8,7 +8,14 @@ calificacion_3= int(input("escribir calificacion3: "))
 #procesos
 suma= calificacion_1 + calificacion_2 + calificacion_3
 promedio= suma/3
-
-
+if(promedio>6 and promedio<=10):
+    print("aprobado")
+elif(promedio== 6):
+    print("epenas aprobado")
+elif(promedio>=0 and promedio<6):
+    print("reprobado")
+#else
+elif(promedio <0 or promedio >10):
+    print("promedio no valido")
 #salida de datos 
 print("Promedio de calificacion es:", round(promedio, 2))
