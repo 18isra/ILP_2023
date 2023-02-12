cc9de56a4355ad4917838a6b3faf67329e69466d
18isra <123257672+18isra@users.noreply.github.com>
2023-02-11 22:02:56 -0600

    CICLOS

---
+++ Ciclos.py
@@ -0,0 +1,14 @@
+#ciclo for
+for i in range(1,5,1):
+    print("hola")
+
+
+j=0                #valor incial
+while(j<=10):      #condicion final
+    print("j")     #proceso
+    j=j+1          #incremento
+
+j=20
+while(j>=0):
+    print(j)
+    j=j-2
