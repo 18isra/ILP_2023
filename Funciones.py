037ef81059143303630a9dc05020a2858424f6d2
18isra <123257672+18isra@users.noreply.github.com>
2023-02-11 22:04:02 -0600

    FUNCIONES

---
+++ Funciones.py
@@ -0,0 +1,33 @@
+#funciones/rutinas/modulos/procedimientos- tareas o acciones a ejecutar
+
+
+
+#declarar o crear 
+def sumar(a,b): #se obtienen o reciben dos parametros 
+    return a+b  #devolviendo el valor de la suma
+
+def restar(x,y):
+    resta=x-y
+    return resta
+
+def multipliacar (d,e):
+    return d*e
+
+def dividir (f,g):
+    return f/g
+
+
+
+#invocar o mandar a llamar una funcion 
+sum=sumar(10,3) #pasar o enviar los valores de los parametros 
+rest=restar(20,4)
+multi=multipliacar(8,8)
+divi=dividir(64,8)
+
+#salida de datos 
+print("la suma es:",sumar(10,3))
+print("la resta es:", restar(20,4))
+print("la multiplicacion es:", multipliacar(8,8))
+print("la division es:", dividir(64,8))
+
+
