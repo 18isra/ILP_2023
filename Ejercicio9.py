b6df5a8772f170b6e2c5da4be170c50111a30f40
18isra <123257672+18isra@users.noreply.github.com>
2023-02-11 21:09:10 -0600

    EJERCICIO 9

---
+++ Ejercicio9.py
@@ -0,0 +1,10 @@
+#EJ  Calcular la nómina para un empleado en el mes de Enero del 2022 conociendosu pago por día de $250.-
+
+pago_por_dia= 250
+
+#datos
+dias_laborados=int(input("dias laborados en enero: "))
+
+#proceso
+salario=pago_por_dia*dias_laborados
+print("salario del empleado en mes de enero: ", salario)
