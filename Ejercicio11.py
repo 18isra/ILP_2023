49b20469e35d5f2e6930e179959bfa6d839c9788
18isra <123257672+18isra@users.noreply.github.com>
2023-02-14 16:13:20 -0600

    EJERCICIO 11

---
+++ Ejercicio11.py
@@ -0,0 +1,9 @@
+#EJ Hacer una función para que imprima un arreglo o lista de 10 elementos numéricos
+
+def print_lista_numeros():
+    numeros=[1,2,3,4,5,6,7,8,9,10]
+    print(numeros)
+
+#llama a la funcion para imprimir la lista
+print_lista_numeros()
+print_lista_numeros
