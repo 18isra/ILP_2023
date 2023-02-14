e6dd9ca2e8e532b8831bc64dc62500718d0329ea
18isra <123257672+18isra@users.noreply.github.com>
2023-02-14 16:13:43 -0600

    EJERCICIO 12

---
+++ Ejercicio12.py
@@ -0,0 +1,11 @@
+# EJ  Crear una lista o arreglo de diccionarios de 5 estados de México con los datos de nombre, población, e
+
+
+estados_nombre = ["Yucatan",        "Michoacan",    "Guerrero",   "Baja California sur",    "Quintana Roo"]
+poblacion=       [2.321,              4749,           3541,           798477,                  1858 ]
+extension=       [43379,               58599,          64281,           73909,              44705]
+
+print("estados nombre:", estados_nombre)
+print("poblacion:", poblacion)
+print("extension:", extension)
+
